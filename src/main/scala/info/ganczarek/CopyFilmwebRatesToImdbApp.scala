b30b84da56337a4376e13dd6ed2ab64c4c39e36c
Rafal Ganczarek <github@ganczarek.info>
2017-01-14 22:14:33 +0000

    Added main app putting everything together

---
+++ src/main/scala/info/ganczarek/CopyFilmwebRatesToImdbApp.scala
@@ -0,0 +1,22 @@
+package info.ganczarek
+
+import info.ganczarek.filmweb.FilmwebClient
+import info.ganczarek.imdb.ImdbClient
+
+object CopyFilmwebRatesToImdbApp {
+
+  def main(args: Array[String]): Unit = {
+    val tmdbApiKey = args(0)
+    val imdbCookieId = args(1)
+    val filmwebLogin = args(2)
+    val filmwebPassword = args(3)
+
+    val filmwebClient = FilmwebClient(filmwebLogin, filmwebPassword)
+    val imdbClient = ImdbClient(imdbCookieId, tmdbApiKey)
+
+    val notFoundMovieRates = imdbClient.submitMovieRates(filmwebClient.userRates()).toList
+    println("Movies that could not be found in IMDb database:")
+    notFoundMovieRates.foreach(println)
+  }
+
+}
